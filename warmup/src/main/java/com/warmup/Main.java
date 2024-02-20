ed0bb30eec9ef6ba34d9d2199b324e6c4f18f493
Photogrammer <khsj15@khu.ac.kr>
2024-02-21 00:23:12 +0900

    init: StringCalculator

--- warmup/src/main/java/com/warmup/Main.java
+++ warmup/src/main/java/com/warmup/Main.java
@@ -1,4 +1,4 @@
-package com.calculator;
+package com.warmup;
 
 public class Main {
     public static void main(String[] args) {
---
+++ warmup/src/main/java/com/warmup/calculator/Calculator.java
@@ -0,0 +1,2 @@
+package com.warmup.calculator;public class Calculator {
+}
